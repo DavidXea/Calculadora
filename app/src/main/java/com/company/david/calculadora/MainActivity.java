7314b1833e3f43d85c16773d112dff463cce22e1
david <david-macielpi@hotmail.com>
2017-05-16 10:53:09 -0300

    Correção de bugs

--- app/src/main/java/com/company/david/calculadora/MainActivity.java
+++ app/src/main/java/com/company/david/calculadora/MainActivity.java
@@ -24,30 +24,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         etOperando1 = (EditText) findViewById(R.id.etOp1);
         etOperando2 = (EditText) findViewById(R.id.etOp2);
         tvResultado = (TextView) findViewById(R.id.tvResult);
-        rGroup = (RadioGroup)findViewById(R.id.radioGroup);
-        bCalcular = (Button) findViewById(R.id.btCalcular);
+
     }
 
     public void onClickCalcular(View quemDisparou) {
 
         final double op1, op2;
 
+        rGroup = (RadioGroup)findViewById(R.id.radioGroup);
+        bCalcular = (Button) findViewById(R.id.btCalcular);
 
         String strOperando1 = etOperando1.getText().toString();
         String strOperando2 = etOperando2.getText().toString();
         etOperando1.setError(null);
         etOperando2.setError(null);
+
         try{
             op1 = Double.parseDouble(strOperando1);
-
         }catch (Exception e){
             etOperando1.setError("Erro ao obter o valor");
             return;
         }
+
         try{
             op2 = Double.parseDouble(strOperando2);
         }catch (Exception e){
@@ -56,18 +57,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         bCalcular.setOnClickListener(new View.OnClickListener() {
-
             double res;
+
             @Override
             public void onClick(View v) {
+
                 int selectedId = rGroup.getCheckedRadioButtonId();
                 rButton = (RadioButton) findViewById(selectedId);
 
-                //Toast.makeText(MainActivity.this, rButton.getText().toString(), Toast.LENGTH_SHORT).show();
-
                 if (getResources().getString(R.string.sum) == rButton.getText().toString()) {
                     res = (op1 + op2);
-
                 } else {
                     if (getResources().getString(R.string.subtraction) == rButton.getText().toString()) {
                         res = (op1 - op2);
@@ -81,16 +80,14 @@ public class MainActivity extends AppCompatActivity {
                         } else {
                             if (getResources().getString(R.string.multiplication) == rButton.getText().toString()) {
                                 res = (op1 * op2);
-                            } else {
-                                Toast.makeText(MainActivity.this, "Selecione uma opção", Toast.LENGTH_SHORT).show();
                             }
                         }
                     }
                 }
-                String resultado = getResources().getString(R.string.result) + res;
-                tvResultado.setText(resultado);
-                Log.d("CALCULO_VALOR", resultado);
-                Toast.makeText(MainActivity.this, "Calculado" , Toast.LENGTH_SHORT).show();
+                    String resultado = getResources().getString(R.string.result) + res;
+                    tvResultado.setText(resultado);
+                    Log.d("CALCULO_VALOR", resultado);
+                    Toast.makeText(MainActivity.this, "Calculado" , Toast.LENGTH_SHORT).show();
             }
         });
     }
